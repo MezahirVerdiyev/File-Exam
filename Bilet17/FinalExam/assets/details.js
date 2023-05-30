5e9052dfe3416fad7d89bc3b2141542561bdec0c
Mezahir <126710071+MezahirVerdiyev@users.noreply.github.com>
2023-05-30 13:35:56 +0400

    Add files via upload

---
+++ Bilet17/FinalExam/assets/details.js
@@ -0,0 +1,38 @@
+let id = new URLSearchParams(window.location.search).get("id");
+const BASE_URL = " http://localhost:5200/users";
+let row = document.querySelector(".row");
+console.log(id);
+async function getAllData() {
+    row.innerHTML = "";
+    const res = await axios(`${BASE_URL}/${id}`);
+    const element = await res.data;
+    row.innerHTML = `
+    <div class="col col-4"></div>
+    <span class="col col-4">
+    <div class="about-card">
+      <img src="${element.photo}" />
+      <h5>${element.title}</h5>
+      <p>${element.description}</p>
+    </div>
+    </span> 
+    <div class="col col-4"></div>
+          `;
+}
+
+getAllData();
+
+let menuBtn = document.querySelector(".menu-btn");
+let headernav = document.querySelector(".headernav");
+let faXmark = document.querySelector(".fa-xmark");
+
+menuBtn.addEventListener("click", () => {
+    headernav.style.display = "flex";
+    faXmark.style.display = "block";
+    menuBtn.style.display = "none";
+});
+
+faXmark.addEventListener("click", () => {
+    headernav.style.display = "none";
+    faXmark.style.display = "none";
+    menuBtn.style.display = "block";
+});
---
+++ Bilet17/FinalExam/assets/fav.js
@@ -0,0 +1,49 @@
+const BASE_URL2 = " http://localhost:5200/favdata";
+
+
+let row = document.querySelector('.row')
+
+async function getFavData() {
+    row.innerHTML = ''
+    const res = await axios(BASE_URL2)
+    const data = await res.data
+    data.forEach(element => {
+        row.innerHTML += `
+    <span class="col col-12 col-lg-4">
+    <div class="about-card">
+      <img src="${element.photo}" />
+      <h5>${element.title}</h5>
+      <p>${element.description}</p>
+      <div class="buttons">
+        <button class="btn btn-danger" onclick=removeData(${element.id},this)>Remove</button>
+      </div>
+    </div>
+  </span> 
+    `
+    });
+}
+
+getFavData()
+
+async function removeData(id) {
+    await axios.delete(`${BASE_URL2}/${id}`)
+    getFavData()
+}
+
+
+let menuBtn = document.querySelector(".menu-btn");
+let burgerModal = document.querySelector(".burger-modal");
+let faXmark = document.querySelector(".fa-xmark");
+
+menuBtn.addEventListener("click", () => {
+    burgerModal.style.display = "flex";
+    faXmark.style.display = "block";
+    menuBtn.style.display = "none";
+});
+
+faXmark.addEventListener("click", () => {
+    burgerModal.style.display = "none";
+    faXmark.style.display = "none";
+    menuBtn.style.display = "block";
+});
+
